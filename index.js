b11316e6f8a15a77f7aaebcac6b7c4d7c07a4d12
Paul <paul@niobium.me>
2017-04-16 00:24:23 +0200

    more idk

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ var express = require("express")
 var app = express()
 
 app.get("/", (req, res) => {
-	res.send("Hello world")
+	res.send(`
+	<form method='post' enctype='multipart/formdata'>
+		<input name="file" type="file">
+	</form>
+	`)
+})
+
+app.post("/", (req, res) => {
+	res.send("Hai")
 })
 
 app.listen(6969)
