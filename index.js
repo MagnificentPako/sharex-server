2b01ae859848a44b19af1f5b3e535e232c7f8446
Paul <paul@niobium.me>
2017-04-16 01:47:48 +0200

    first version done

--- index.js
+++ index.js
@@ -23,34 +23,44 @@ app.get('/', (req, res) => {
 
 app.post('/', upload.single('file'), (req, res) => {
 	// res.send(JSON.stringify(req.body) + "\n" + JSON.stringify(req.file))
-  	const name = randomstring.generate(6)
-	const newpath = 'uploads/' + name + '.' + mime.extension(req.file.mimetype)
-  fs.writeFile(newpath, req.file.buffer, (err) => {
-    if (err) throw err
-  })
 
-  res.send(config.basePath + name)
+  let sent = false
+
+  if (config.users[req.body.username] !== undefined) {
+	  if (config.users[req.body.username] === req.body.password) {
+    const name = randomstring.generate(6)
+    const newpath = 'uploads/' + name + '.' + mime.extension(req.file.mimetype)
+    fs.writeFile(newpath, req.file.buffer, (err) => {
+      if (err) throw err
+    })
+    sent = true
+    res.send(config.basePath + name)
+  }
+  }
+  if (!sent) {
+    res.send('GET OUT NOW')
+  }
 })
 
 const options = {
-	root: __dirname + "/uploads/",
-	dotfiles: "deny",
-	headers: {
-		"x-timestamp": Date.now(),
-		"x-sent": true
-	}
+  root: __dirname + '/uploads/',
+  dotfiles: 'deny',
+  headers: {
+    'x-timestamp': Date.now(),
+    'x-sent': true
+  }
 }
 
-app.get("/:img", (req, res) => {
-	fs.readdir("uploads", (err, files) => {
-		files.forEach( file => {
-			if(file.startsWith(req.params.img)) {
-				res.sendFile(file, options, err => {
-					if (err) throw err;
-				})
-			}
-		})
-	})
+app.get('/:img', (req, res) => {
+  fs.readdir('uploads', (err, files) => {
+    files.forEach(file => {
+      if (file.startsWith(req.params.img)) {
+        res.sendFile(file, options, err => {
+          if (err) throw err
+        })
+      }
+    })
+  })
 })
 
 app.listen(6969)
